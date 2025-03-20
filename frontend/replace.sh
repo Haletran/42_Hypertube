82f22b80c8f3b6c07b911fe5831b643c2d9e05e7
Baptiste-Pasquier <bpasquier123@gmail.com>
2025-03-20 18:16:25 +0100

    Delete frontend/replace.sh

--- frontend/replace.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-CURRENT_IP="backend:3333"
-SEARCH_DIR="."
-
-echo "Replacing 'localhost' with '$CURRENT_IP' in files under '$SEARCH_DIR'..."
-find "$SEARCH_DIR" -type f ! -name "$(basename "$0")" ! -path "*/crypto/*" -exec sed -i "s/localhost:3333/$CURRENT_IP/g" {} +
-echo "Replacement completed!"
